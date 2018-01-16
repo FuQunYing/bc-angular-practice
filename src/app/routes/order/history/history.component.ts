44bc2df55fb7aad3bd0afa8bd8486fd99b8a054a
FuQunYing <fqylovae@outlook.com>
2018-01-16 13:45:19 +0800

    order/history updated

--- src/app/routes/order/history/history.component.ts
+++ src/app/routes/order/history/history.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../../../service/service.module';
+import { NzModalService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-history',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
-
-  constructor() { }
+  orders: Array<any> = [];
+  loading: Boolean = false;
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+    this.orderService.getAllOrder([4])
+          .then(res => {
+            this.orders = res.json().result.result.detail;
+            this.loading = res.json().result.result.count ? true : false;
+          });
   }
 
 }
